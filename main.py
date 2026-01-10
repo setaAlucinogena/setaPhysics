96bf4636779172e48ea2247577313501f2d49357
seta <>
2026-01-10 01:09:29 +0100

    .

--- main.py
+++ main.py
@@ -3,8 +3,16 @@ from  physic_object import *
 
 app = Ursina()
 
-po = PhysicalObject(position = (0,0,0),model = "cube", phyisics_type=PhysicsType.MOBILE,scale = 1)
+po = PhysicalObject(position = (0,0,0), mass = 5,model = "cube", phyisics_type=PhysicsType.MOBILE,scale = 1)
+ground = PhysicalObject(position = (0,-2,0), model = "cube", phyisics_type=PhysicsType.INAMOBIBLE,scale = (10,.5,10))
 
+
+def input(key):
+    if key == "j":
+        po.movement_vector[1] += 90
+
+#def update():
+#    po.position+=po.left*time.dt*3*held_keys["a"]
 
 EditorCamera()
 app.run()
--- physic_object.py
+++ physic_object.py
@@ -9,9 +9,11 @@ class PhysicsType(Enum):
 
 class PhysicalObject(Entity):
     gravity = True
-    gravity_vector = Vec3(0,-9.8,0)
+    gravity_vector = Vec3(0,-9.81,0)
+    #should I include a parameter for the terminal velocity to avoid clipping??
 
-    def __init__(self, position, model, phyisics_type, scale):
+
+    def __init__(self, position, model, phyisics_type, scale, mass = -1):
         super().__init__(
             position = position,
             scale = scale,
@@ -27,18 +29,73 @@ class PhysicalObject(Entity):
         self.grounded = True
         self.movement_vector = Vec3(0,0,0)
 
-    def update(self):
-        if self.physics_type == PhysicsType.MOBILE:
-            self.position = self.movement_vector*time.dt
-            if PhysicalObject.gravity:
-                raycast(self.position, PhysicalObject.gravity_vector.normalized(), ignore=(self,), distance=(self.scale[
-                    int(abs(PhysicalObject.gravity_vector.normalized()[0])*0)+
+        self.mass = mass
+        if self.physics_type == PhysicsType.INAMOBIBLE:
+            self.mass = -1 #gotta look up how to get the biggest number in python, while I dont do it, -1 will do as a rather symbolic value
+
+    def getFallDirection(self):
+        return (
+            int(abs(PhysicalObject.gravity_vector.normalized()[0])*0)+
                     int(abs(PhysicalObject.gravity_vector.normalized()[1]))+
                     int(abs(PhysicalObject.gravity_vector.normalized()[2])*2)
-                    ]/2), debug=True)
+            )
+
+
+    #puts grounded to True, meaning that the object is apoyado in something stable. 
+    #also, if there's still movement applied by the gravity in the movement vector, then it = 0.
+    def land(self):
+        print("land")
+        self.grounded = True
+
+        #self.y+=.1
+        #self.movement_vector[1] = 0
+        #self.grounded = True
+        if self.movement_vector[self.getFallDirection()]:
+            tmp1 = self.gravity_vector[self.getFallDirection()]
+            tmp2 = self.movement_vector[self.getFallDirection()]
+            if abs(tmp1) != tmp1: #if gravity drags you to the negative direction of the axis: (eg: "regular" gravity always drags you to -y)
+                if tmp2 < 0:
+                    self.movement_vector[self.getFallDirection()] = 0
+            elif abs(tmp1) == tmp1: #if gravity drags you to the positive direction of the axis:
+                if tmp2 > 0:
+                    self.movement_vector[self.getFallDirection()] = 0
+
+        self.position[self.getFallDirection()] = self.ground_ray.world_point[self.getFallDirection()]+.1
+        
+
+    def fall(self):
+        self.grounded = False
+        self.movement_vector += PhysicalObject.gravity_vector
+
+    def update(self):
+        if self.physics_type == PhysicsType.MOBILE:
+            self.position += self.movement_vector*time.dt
+            if PhysicalObject.gravity:
+                self.ground_ray = raycast(self.position, PhysicalObject.gravity_vector.normalized(), ignore=(self,), 
+                                          distance=((self.scale[self.getFallDirection()]/2)+1), debug=True)
+
+                if self.ground_ray.hit:
+                    
+                    if not self.grounded:
+                        self.land()
+                else:
+                    
+                    self.fall()
+
+                #if self.ground_ray.hit:
+                #    for entity in self.ground_ray.entities:
+                #        if (entity.physics_type == PhysicsType.INAMOBIBLE or
+                #            entity.physics_type == PhysicsType.MOBILE
+                #            ):
+                #            if entity.grounded:#si lo q toca esta recolzant-se en algo:
+                #                self.land()
+
+                #if not self.grounded:
+                #    self.fall()
 
 
 
 
-        elif self.phyisics_type == PhysicsType.INAMOBIBLE:
+
+        elif self.physics_type == PhysicsType.INAMOBIBLE:
             pass
